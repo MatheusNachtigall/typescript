b601594a0bd52099b014ec4c04da61e1871061b0
MatheusNachtigall <matheusgnachtigall@gmail.com>
2023-05-10 13:30:08 -0300

    feat: :construction: "wrong" (old) way of doing AJAX

--- avancado/public/js/test.js
+++ avancado/public/js/test.js
@@ -1,5 +1,7 @@
-define(["require", "exports", "./module1"], function (require, exports) {
+define(["require", "exports", "http/post-http", "./module1"], function (require, exports, post_http_1) {
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
+    var postHttpClass = new post_http_1.PostHttp();
+    postHttpClass.query();
 });
 //# sourceMappingURL=test.js.map
--- avancado/src/test.ts
+++ avancado/src/test.ts
@@ -1,1 +1,5 @@
+import { PostHttp } from "http/post-http";
 import "./module1";
+
+let postHttpClass: PostHttp = new PostHttp();
+postHttpClass.query();
